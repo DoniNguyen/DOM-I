c70c88968cb584fe5848800b1e9e87856c9fa55c
DoniNguyen <mew3sg@gmail.com>
2019-10-29 17:33:12 -0500

    finished

--- js/index.js
+++ js/index.js
@@ -40,3 +40,57 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const nav = document.getElementsByTagName('a');
+Array.from(nav)
+
+nav[0].textContent = "Services";
+nav[1].textContent = "Product";
+nav[2].textContent = "Vision";
+nav[3].textContent = "Features";
+nav[4].textContent = "About";
+nav[5].textContent = "Contact";
+
+const headerText = document.querySelector(".cta-text")
+headerText.innerHTML=`<h1>DOM<br> Is<br> Awesome</h1>
+<button>Get Started</button>`
+
+const headerImg = document.getElementById("cta-img");
+headerImg.setAttribute('src', 'img/header-img.png')
+
+const textContent = document.querySelectorAll(".text-content")
+
+textContent[0].innerHTML=`<h4>Features</h4>
+<p>Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.</p>
+</div>`
+textContent[1].innerHTML=`<h4>About</h4>
+<p>About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.</p>`
+textContent[2].innerHTML=`<h4>Services</h4>
+<p>Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.</p>`
+
+const middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', 'img/mid-page-accent.jpg')
+
+textContent[3].innerHTML=`<h4>Product</h4>
+<p>Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.</p>`
+textContent[4].innerHTML=`<h4>Vision</h4>
+<p>Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.</p>`
+
+const bottom = document.querySelector(".contact");
+bottom.innerHTML=`
+<h4>Contact</h4>
+
+<p>123 Way 456 Street<br>
+Somewhere, USA</p>
+
+<p>1 (888) 888-8888</p>
+
+<p>sales@greatidea.io</p>
+`;
+
+const foot = document.getElementsByTagName("footer");
+foot[0].textContent = "Copyright Great Idea! 2018";
+
+
+
+
